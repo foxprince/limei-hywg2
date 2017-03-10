b4512bab523e634a7061727b9ee15afeac14597b
foxprince <9600348@qq.com>
2017-03-10 14:52:46 +0800

    ddd

--- cn/whylumia.php
+++ cn/whylumia.php
@@ -1,29 +1,26 @@
-		
-		<div class="text-title"><span>利美优势</span></div>
-		<div class="text-top">
-			<img class="ring" src="../images/ring.png">
-		</div>
-   </div>
-   <div class="div_text">
-		<div class="text_in"> 
-<div  class="container-fluid">
-	<div class="row">
-		<div class="subnavi_box-1 col-xs-12 col-sm-12 col-md-3">
-		</div>
-		<div class="col-xs-12 col-sm-12 col-md-9">
-			<div style="text-align:left; margin-bottom:20px;margin-top:300px;"><strong style="color:#900;font-size:18px; font-weight:bold;">作为世界上80-90%钻石的来源地，安特卫普钻交所是您理想的选购平台，而利美钻石做为华人的购钻渠道，将竭诚为您的选择提供专业服务。</strong></div>
-			<p><strong>成为钻交所会员是从业者专业实力的展现</strong>。在安特卫普钻交所申请成为会员，从业者不仅需要具有专业学历和相关资格证书，还有提供资金担保和同行推荐信——包括7家大型钻石贸易公司对创始人专业能力和信誉的推荐。此外，申请人还要具有良好的历史交易记录，不能有其他钻交所会员给与的负面评论。</p>
-			<p>做为钻交所成员，如若发生任何拖欠货款、出售假货或出售未声明的人工合成钻石的情况，将终身失去在世界WFDB钻石联盟里任何其它钻交中心内交易的权利。换句话说，如果为了一时之利而做了违背信誉的行为，将被行业永久除名。</p>
-			
-			<p style="text-align:left; margin:12px 0;"><img style="width:100%" src="../images/sitepictures/limeiwhy.jpg" /></p>
-			
-			<p>作为安特卫普的传统行业，钻石行业长期被犹太人和印度人主要控制。<strong>中国企业家进入当地钻交所，靠的不仅仅是扎实的专业知识，更需要凭借是正直的人品和优秀的经营头脑。</strong></p>
-			<p>BELGEM公司在成立三年多来，和钻交所里不同文化背景的同行都建立了友好的合作关系，不仅具有稳固的行业声誉，更为其客户开辟了广泛的货源。钻交所成员间通过内部网络平台实行资源共享，一家会员公司的货源即是其它所有会员公司的货源。</p>
-			<p style="text-align:right;"><br /><br /></p>
-		</div>
+<div class="text-title"><span>利美优势</span></div>
+	<div class="text-top">
+		<img class="ring" src="../images/ring.png">
 	</div>
 </div>
-</div>
+<div class="div_text">
+	<div class="text_in"> 
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-9">
+					<div style="text-align:left; margin-bottom:20px;margin-top:300px;"><strong style="color:#900;font-size:18px; font-weight:bold;">作为世界上80-90%钻石的来源地，安特卫普钻交所是您理想的选购平台，而利美钻石做为华人的购钻渠道，将竭诚为您的选择提供专业服务。</strong></div>
+					<p><strong>成为钻交所会员是从业者专业实力的展现</strong>。在安特卫普钻交所申请成为会员，从业者不仅需要具有专业学历和相关资格证书，还有提供资金担保和同行推荐信——包括7家大型钻石贸易公司对创始人专业能力和信誉的推荐。此外，申请人还要具有良好的历史交易记录，不能有其他钻交所会员给与的负面评论。</p>
+					<p>做为钻交所成员，如若发生任何拖欠货款、出售假货或出售未声明的人工合成钻石的情况，将终身失去在世界WFDB钻石联盟里任何其它钻交中心内交易的权利。换句话说，如果为了一时之利而做了违背信誉的行为，将被行业永久除名。</p>
+					
+					<p style="text-align:left; margin:12px 0;"><img style="width:100%" src="../images/sitepictures/limeiwhy.jpg" /></p>
+					
+					<p>作为安特卫普的传统行业，钻石行业长期被犹太人和印度人主要控制。<strong>中国企业家进入当地钻交所，靠的不仅仅是扎实的专业知识，更需要凭借是正直的人品和优秀的经营头脑。</strong></p>
+					<p>BELGEM公司在成立三年多来，和钻交所里不同文化背景的同行都建立了友好的合作关系，不仅具有稳固的行业声誉，更为其客户开辟了广泛的货源。钻交所成员间通过内部网络平台实行资源共享，一家会员公司的货源即是其它所有会员公司的货源。</p>
+					<p style="text-align:right;"><br /><br /></p>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 <script type="text/javascript">
 	$('document').ready(function(){
