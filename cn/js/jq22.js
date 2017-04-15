6e146bd1f3d4d5503ab4cf288fff72bcf3577d34
foxprince <9600348@qq.com>
2017-04-15 10:10:38 +0800

    f

--- cn/js/jq22.js
+++ cn/js/jq22.js
@@ -18,10 +18,10 @@ for (var i=0; i<thumbs.length; i++) {
     thumbs[i].onmouseover = function () {jQuery('main_img').src=this.rel; jQuery('main_img').link=this.link;};
     thumbs[i].onclick = function () {location = this.link}
 }
-// jQuery('main_img').onclick = function () {location = this.link;}
-// jQuery('gotop').onmouseover = function() {this.src = ipath + 'gotop2.gif'; MyMar=setInterval(gotop,speed);}
-// jQuery('gotop').onmouseout = function() {this.src = ipath + 'gotop.gif'; clearInterval(MyMar);}
-// jQuery('gobottom').onmouseover = function() {this.src = ipath + 'gobottom2.gif'; MyMar=setInterval(gobottom,speed);}
-// jQuery('gobottom').onmouseout = function() {this.src = ipath + 'gobottom.gif'; clearInterval(MyMar);}
+jQuery('main_img').onclick = function () {location = this.link;}
+jQuery('gotop').onmouseover = function() {this.src = ipath + 'gotop2.gif'; MyMar=setInterval(gotop,speed);}
+jQuery('gotop').onmouseout = function() {this.src = ipath + 'gotop.gif'; clearInterval(MyMar);}
+jQuery('gobottom').onmouseover = function() {this.src = ipath + 'gobottom2.gif'; MyMar=setInterval(gobottom,speed);}
+jQuery('gobottom').onmouseout = function() {this.src = ipath + 'gobottom.gif'; clearInterval(MyMar);}
 function gotop() {jQuery('showArea').scrollTop-=spec;}
 function gobottom() {jQuery('showArea').scrollTop+=spec;}
