d256d867dc2f33560d130169b0d35ac2fc519ee9
foxprince <9600348@qq.com>
2017-07-23 17:46:24 +0800

    dfdf

--- cn/header.php
+++ cn/header.php
@@ -28,6 +28,29 @@ require_once('log.php');
 	<script type="text/javascript" src="./js/jquery.1.11.3.min.js"></script>
 	<script type="text/javascript" src="./js/text.js"></script>
 	<script type="text/javascript" src="./js/js.cookie.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-84729490-1', 'auto');
+  ga('send', 'pageview');
+</script>
+<script>
+<!--百度seo自动推送 -->
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
 	<script>
 	// Generate four random hex digits.  
 	function S4() {  
