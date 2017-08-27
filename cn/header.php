6fb256384784e03a4eb63fa74736b7b255cd6823
foxprince <9600348@qq.com>
2017-08-27 23:30:51 +0800

    电饭锅

--- cn/header.php
+++ cn/header.php
@@ -16,7 +16,7 @@ require_once('log.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<meta charset="utf-8" />
-	<meta content="比利时钻石,安特卫普钻石" name="keywords" />
+	<meta content="比利时钻石,安特卫普钻石,钻石购买,钻交所" name="keywords" />
 	<meta name="description" content="利美钻石主营产品：比利时钻石，安特卫普钻石，利美钻石婚戒定制专家，为您专业定制独一无二的钻戒，每颗钻石都配有国际钻石证书，清晰的解释，贴心的建议和优惠的价格是利美对您的承诺，作为华人的购钻渠道，利美钻石将竭诚为您的选择提供最专业的服务。"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="Pragma" content="no-cache">
