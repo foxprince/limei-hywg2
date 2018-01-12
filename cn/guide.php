2b555dfb6ae5b7f95eec3836bdec26bc56fedea3
foxprince <9600348@qq.com>
2018-01-12 14:22:46 +0800

    ddd

--- cn/guide.php
+++ cn/guide.php
@@ -1,4 +1,5 @@
-﻿<?
-header("location: http://www.lumiagem.be/cn/intro.php?c=order");
-exit;
-?>
+﻿<html>
+<head>
+<meta http-equiv="refresh" content="0;url=http://www.lumiagem.be/cn/intro.php?c=order">
+</head>
+</html>
