6045e71e5cda44be16828e0d934407805f3773ac
foxprince <9600348@qq.com>
2018-02-28 23:01:26 +0800

    sss

--- cn/action.php
+++ cn/action.php
@@ -249,6 +249,26 @@ if($_REQUEST['action']) {
 				} else {
 					logger( 'Message has been sent');
 				}
+				//发短信
+				$encode='UTF-8';  //页面编码和短信内容编码为GBK。重要说明：如提交短信后收到乱码，请将GBK改为UTF-8测试。如本程序页面为编码格式为：ASCII/GB2312/GBK则该处为GBK。如本页面编码为UTF-8或需要支持繁体，阿拉伯文等Unicode，请将此处写为：UTF-8
+				$username='yhhd';  //用户名
+				$password_md5='7240835ee7502289c76f5e9bd4cddb8d';  //32位MD5密码加密，不区分大小写
+				$apikey='ec315b55a76455ddd6789e192b8b670a';  //apikey秘钥（请登录 http://m.5c.com.cn 短信平台-->账号管理-->我的信息 中复制apikey）
+				$content=str_replace("<b>", "", $content);$content=str_replace("</b>", "", $content);
+				$content=str_replace("<br/>", "", $content);
+				$content='您好，您的验证码是：12345【美联】';  //要发送的短信内容，特别注意：签名必须设置，网页验证码应用需要加添加【图形识别码】。
+				$content = iconv("GBK","UTF-8",$content);
+				$contentUrlEncode = urlencode($content);//执行URLencode编码  ，$content = urldecode($content);解码
+				$result = sendSMS($username,$password_md5,$apikey,$tel,$contentUrlEncode,$encode);  //进行发送
+				
+				if(strpos($result,"success")>-1) {
+					//提交成功
+					//逻辑代码
+				} else {
+					//提交失败
+					//逻辑代码
+				}
+				echo $result;  //输出result内容，查看返回值，成功为success，错误为error，（错误内容在上面有显示）
 			}else{
 				$feedbackwords='请检查您输入的数据。如果还存在问题请联系我们客服微信号limeikefu。';
 			}
@@ -636,5 +656,37 @@ function diamondShapeDesc($shape) {
 			break;
 	}
 	return $shape_TXT;	
+}
+//发送接口
+function sendSMS($username,$password_md5,$apikey,$mobile,$contentUrlEncode,$encode)
+{
+	//发送链接（用户名，密码，apikey，手机号，内容）
+	$url = "http://m.5c.com.cn/api/send/index.php?";  //如连接超时，可能是您服务器不支持域名解析，请将下面连接中的：【m.5c.com.cn】修改为IP：【115.28.23.78】
+	$data=array
+	(
+			'username'=>$username,
+			'password_md5'=>$password_md5,
+			'apikey'=>$apikey,
+			'mobile'=>$mobile,
+			'content'=>$contentUrlEncode,
+			'encode'=>$encode,
+	);
+	$result = curlSMS($url,$data);
+	//print_r($data); //测试
+	return $result;
+}
+function curlSMS($url,$post_fields=array())
+{
+	$ch=curl_init();
+	curl_setopt($ch,CURLOPT_URL,$url);//用PHP取回的URL地址（值将被作为字符串）
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);//使用curl_setopt获取页面内容或提交数据，有时候希望返回的内容作为变量存储，而不是直接输出，这时候希望返回的内容作为变量
+	curl_setopt($ch,CURLOPT_TIMEOUT,30);//30秒超时限制
+	curl_setopt($ch,CURLOPT_HEADER,1);//将文件头输出直接可见。
+	curl_setopt($ch,CURLOPT_POST,1);//设置这个选项为一个零非值，这个post是普通的application/x-www-from-urlencoded类型，多数被HTTP表调用。
+	curl_setopt($ch,CURLOPT_POSTFIELDS,$post_fields);//post操作的所有数据的字符串。
+	$data = curl_exec($ch);//抓取URL并把他传递给浏览器
+	curl_close($ch);//释放资源
+	$res = explode("\r\n\r\n",$data);//explode把他打散成为数组
+	return $res[2]; //然后在这里返回数组。
 }
 ?>
