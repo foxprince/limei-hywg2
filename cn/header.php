b610e86fcf897d417bca39e101769651512c046d
foxprince <9600348@qq.com>
2018-07-18 07:20:07 +0800

    reirect index

--- cn/header.php
+++ cn/header.php
@@ -28,6 +28,54 @@ require_once('log.php');
 	<script type="text/javascript" src="./js/jquery.1.11.3.min.js"></script>
 	<script type="text/javascript" src="./js/text.js"></script>
 	<script type="text/javascript" src="./js/js.cookie.js"></script>
+	<script type="text/javascript">
+	var bForcepc = fGetQuery("dv") == "pc";
+	function fBrowserRedirect() {
+		var sUserAgent = navigator.userAgent.toLowerCase();
+		var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+		var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+		var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+		var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+		var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+		var bIsAndroid = sUserAgent.match(/android/i) == "android";
+		var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+		var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+		if (bIsIpad) {
+			var sUrl = location.href;
+			if (!bForcepc) {
+				window.location.href = "/m/";
+			}
+		}
+		else if (bIsIphoneOs || bIsAndroid) {
+			var sUrl = location.href;
+			if (!bForcepc) {
+				window.location.href = "/m/";
+				return;
+			}
+		}
+		else if (bIsMidp || bIsUc7 || bIsUc || bIsCE || bIsWM) {
+			var sUrl = location.href;
+			if (!bForcepc) {
+				window.location.href = "/m/";
+			}
+		}
+		else
+			window.location.href="/cn/index.php"
+	}
+	function fGetQuery(name) {//获取参数值 
+		var sUrl = window.location.search.substr(1);
+		var r = sUrl.match(new RegExp("(^|&)" + name + "=([^&]*)(&|$)"));
+		return (r == null ? null : (r[2]));
+	}
+	function fShowVerBlock() {
+		if (bForcepc) {
+			document.getElementByIdx_x("dv_block").style.display = "block";
+		} else {
+			document.getElementByIdx_x("ad_block").style.display = "block";
+		}
+	}
+	fBrowserRedirect();
+</script>
 <!-- 
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
