d62b1b1769620fd4b3ab9e09d91cc8bc72b96f49
foxprince <9600348@qq.com>
2018-10-21 19:17:13 +0800

    fff

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+header("Content-type: text/html; charset=utf-8");
+static $realip = NULL;
+$url='http://freeapi.ipip.net/'.get_ip();
+try{
+	$html = file_get_contents($url);
+	echo $html;
+	$arr = json_decode($html);
+	if($arr[0]=='中国') {
+		header("Location: http://cn.lumiagem.com/index.html");
+		exit;
+	}
+	else {
+		header("Location: http://www.lumiagem.com/index.html");
+		exit;
+	}
+}catch(Exception $e){
+	header("Location: http://www.lumiagem.com/index.html");
+	exit;
+}
+function get_ip(){
+	if($realip !== NULL){
+		return $realip;
+	}
+	//判断服务器是否允许$_SERVER
+	if(isset($_SERVER)){
+		if(isset($_SERVER[HTTP_X_FORWARDED_FOR])){
+			$realip = $_SERVER[HTTP_X_FORWARDED_FOR];
+		}elseif(isset($_SERVER[HTTP_CLIENT_IP])) {
+			$realip = $_SERVER[HTTP_CLIENT_IP];
+		}else{
+			$realip = $_SERVER[REMOTE_ADDR];
+		}
+	}else{
+		//不允许就使用getenv获取
+		if(getenv("HTTP_X_FORWARDED_FOR")){
+			$realip = getenv( "HTTP_X_FORWARDED_FOR");
+		}elseif(getenv("HTTP_CLIENT_IP")) {
+			$realip = getenv("HTTP_CLIENT_IP");
+		}else{
+			$realip = getenv("REMOTE_ADDR");
+		}
+	}
+
+	return $realip;
+}
+?>
