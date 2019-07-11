8eea0b4e24e618192b663f255aaf10e1444be8a7
foxprince <9600348@qq.com>
2019-07-11 19:49:54 +0800

    d

--- cn/log.php
+++ cn/log.php
@@ -7,15 +7,15 @@ function traceHttp()
 function logger($log_content)
 {
 	date_default_timezone_set("Asia/Shanghai");
-	if(isset($_SERVER['HTTP_APPNAME'])){   //SAE
+	/*if(isset($_SERVER['HTTP_APPNAME'])){   //SAE
         sae_set_display_errors(false);
         sae_debug($log_content);
         sae_set_display_errors(true);
-    }else{ //LOCAL
+    }else{*/
         $max_size = 50000000;
         $log_filename = "log.txt";
         if(file_exists($log_filename) and (abs(filesize($log_filename)) > $max_size)){unlink($log_filename);}
         file_put_contents($log_filename, date('Y-m-d H:i:s ').$log_content."\r\n", FILE_APPEND);
-    }
+    //}
 }
 ?>
