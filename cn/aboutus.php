fd46291a494ff711f549c6db5876cbbaeb096d68
foxprince <9600348@qq.com>
2019-07-11 14:46:32 +0800

    ff

--- cn/aboutus.php
+++ cn/aboutus.php
@@ -14,15 +14,15 @@
 				<p>&nbsp;</p>
 				<p>中国地址：深圳市罗湖区南湖街道建设路东方广场1112</p>
           		<p>中国电话：+86 13905822677</p>
-				<p><a href="tel:+32(0)36897394"><img class="fa" src="images/icon-phone.png" alt="电话"/>：+32（0）3689 7394</a></p>
-				<p><a href="mailto:info@lumiagem.com"><img class="fa" src="images/icon-email.png" alt="邮箱"/>：info@lumiagem.com</a></p>
-				<p><img class="fa" src="images/icon-wechat.png" alt="微信"/>：请扫描右侧二维码</p>
 				<p>
 					<a href="http://map.baidu.com/?latlng=51.215356,4.420171&title=我们在这里&content=Pelikaanstraat 62,2018 Antwerp Belgium&autoOpen=true&l" >
 					<img class="fa" src="images/icon-address.png" alt="地址"/>：Pelikaanstraat 62,2018 Antwerp Belgium
 					</a>
-				</p><p>&nbsp;</p>
+				</p>
 				<p><a href="intro.php?c=route"><img class="fa" src="images/trafficGuide.png" alt="邮箱"/>：交通指引及注意事项</a></p>
+				<p><a href="tel:+32(0)36897394"><img class="fa" src="images/icon-phone.png" alt="电话"/>：+32（0）3689 7394</a></p>
+				<p><a href="mailto:info@lumiagem.com"><img class="fa" src="images/icon-email.png" alt="邮箱"/>：info@lumiagem.com</a></p>
+				<p><img class="fa" src="images/icon-wechat.png" alt="微信"/>：请扫描右侧二维码</p>
 				</div>
 			</div>
 			<div class="aboutus_weixin"></div>
