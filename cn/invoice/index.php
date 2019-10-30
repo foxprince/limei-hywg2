05ebda72801e770c25af1d9db88dce5fa1dee4a4
foxprince <9600348@qq.com>
2019-10-30 21:30:12 +0800

    d

--- cn/invoice/index.php
+++ cn/invoice/index.php
@@ -280,7 +280,7 @@
    			     	    <label class='col-sm-2 col-xs-3  control-label'>Jewelry</label>\
    			     	    <div class='col-sm-10 col-xs-9'>\
    			     	    <select id='form_jewerly' class='jewerly form-control'>\
-   			     	    <option value='Ring' "+(j.jewerly=='Ring'?'selected':'')+">Ring</option><option value='Necklace' "+(j.jewerly=='Necklace'?'selected':'')+">Necklace</option><option value='Earring' "+(j.jewerly=='Earring'?'selected':'')+">Earring</option><option value='Diamond Ring' "+(j.jewerly=='Diamond Ring'?'selected':'')+">Diamond Ring</option><option value='Diamond Necklace' "+(j.jewerly=='Diamond Necklace'?'selected':'')+">Diamond Necklace</option><option value='Diamond Earring' "+(j.jewerly=='Diamond Earring'?'selected':'')+">Diamond Earring</option>\
+   			     	    <option value='Ring' "+(j.jewerly=='Ring'?'selected':'')+">Ring</option><option value='Necklace' "+(j.jewerly=='Necklace'?'selected':'')+">Necklace</option><option value='Earring' "+(j.jewerly=='Earring'?'selected':'')+">Earring</option><option value='Bracelet' "+(j.jewerly=='Bracelet'?'selected':'')+">Earring</option><option value='Diamond Ring' "+(j.jewerly=='Diamond Ring'?'selected':'')+">Diamond Ring</option><option value='Diamond Necklace' "+(j.jewerly=='Diamond Necklace'?'selected':'')+">Diamond Necklace</option><option value='Diamond Earring' "+(j.jewerly=='Diamond Earring'?'selected':'')+">Diamond Earring</option><option value='Diamond Bracelet' "+(j.jewerly=='Diamond Bracelet'?'selected':'')+">Diamond Bracelet</option>\
    			     	    </select>\
    			     	    </div></div><div class='form-group clearfix'>\
    			     	    <label class='col-sm-2 col-xs-3  control-label'>Price</label>\
@@ -461,7 +461,8 @@
    			     	    <option value='Ring'"+(j.jewerly=='Ring' ? 'selected':'')+">Ring</option>\
    			     	    <option value='Necklace'"+(j.jewerly=='Necklace' ? 'selected':'')+">Necklace</option>\
    			     	    <option value='Earring'"+(j.jewerly=='Earring' ? 'selected':'')+">Earring</option>\
-   			     	    </select>\
+   			     		<option value='Bracelet'"+(j.jewerly=='Bracelet' ? 'selected':'')+">Bracelet</option>\
+			     	    </select>\
    			     	    </div></div><div class='form-group clearfix'>\
    			     	    <label class='col-sm-2 col-xs-3  control-label'>Price</label>\
    			     	    <div class='col-sm-10 col-xs-9'> \
