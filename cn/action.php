308831df276e4cf51b867456ce49ddbda7c35176
foxprince <9600348@qq.com>
2021-03-29 21:25:44 +0800

    d

--- cn/action.php
+++ cn/action.php
@@ -510,6 +510,7 @@ if($_REQUEST['action']) {
 							$item["polish"], $item["symmetry"],$item["price"],$item["jewerly"], $item["material"],
 							$item["jewerly_price"],$item["jewerly_color"],$item["raw_price"]
 					));
+					logger($result);
 				}
 			}
 			echo $obj['id'].','.$obj['invoice_no'];
@@ -575,6 +576,7 @@ if($_REQUEST['action']) {
 							$item["polish"], $item["symmetry"],$item["price"],$item["jewerly"], $item["material"],
 							$item["jewerly_price"],$raw_price
 					));
+					logger($result);
 				}
 			}
 			echo $transactionId.','.$transactionNo;
