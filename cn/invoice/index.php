15215739edd5ecb1a8da38e525278d2d026343ef
zj <9600348@qq.com>
2023-04-05 21:14:08 +0800

    delete taiwan tv

--- cn/invoice/index.php
+++ cn/invoice/index.php
@@ -467,7 +467,7 @@
    			     	    <div class='form-group clearfix'>\
    			     	    <label class='col-sm-2 col-xs-3  control-label'>Jewelry</label>\
    			     	    <div class='col-sm-10 col-xs-9'>\
-   			     	    <input id='type' type='text' name='jewerlyType' list='typelist' value='+j.jewerly+'>\
+   			     	    <input id='type' type='text' name='jewerlyType' list='typelist' value='"+j.jewerly+"'>\
                                    			     	    <datalist id='typelist'>\
                                                         　　<option value='Ring'>Ring</option><option value='Necklace'>Necklace</option><option value='Earring'>Earring</option><option value='Diamond Ring'>Diamond Ring</option><option value='Diamond Necklace'>Diamond Necklace</option><option value='Diamond Earring'>Diamond Earring</option>\
                                                         </datalist>\
